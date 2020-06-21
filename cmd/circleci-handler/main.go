574e74500d6d3158acf768531bed8473ff5beb30
eitah <eli@spantree.net>
2020-06-21 12:34:35 -0500

    WIP Commit at eod

--- cmd/circleci-handler/main.go
+++ cmd/circleci-handler/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
@@ -14,6 +16,8 @@ import (
 
 // Config contains all the configurable env variables absorbed from the environment. split_words turns camel in to snake case.
 type Config struct {
+	SlackWebhookURL string `split_words:"true" envconfig:"SLACK_WEBHOOK_URL"`
+
 	// logging
 	LogLevel string `split_words:"true" default:"info"`
 	LogJSON  bool   `split_words:"true" default:"true"`
@@ -45,12 +49,55 @@ func init() {
 
 func main() {
 	// todo I want header auth to happen in the gateway not in the lambda, so I bypass using that config value for now
-	lambda.Start(handler)
+	// lambda.Start(handler)
+	eliTesting()
+}
+
+type postBody struct {
+	text string
+}
+
+func eliTesting() (string, error) {
+	spew.Dump("eli is the best!")
+	// curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}'
+	var buf bytes.Buffer
+	p := postBody{text: "Hello, Pipeline!"}
+
+	err := json.NewEncoder(&buf).Encode(p)
+	if err != nil {
+		return "", err
+	}
+	spew.Dump("eprepost")
+
+	response, err := http.Post(config.SlackWebhookURL, "application/json", &buf)
+	if err != nil {
+		spew.Dump("err", err)
+
+		return "", err
+	}
+	defer response.Body.Close()
+	spew.Dump("post")
+
+	return "success", nil
 }
 
 func handler(ctx context.Context, event events.APIGatewayProxyRequest) (string, error) {
 	spew.Dump("eli is the best!")
-	return "", nil
+	// curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}'
+	var buf bytes.Buffer
+	p := postBody{text: "Hello, Pipeline!"}
+
+	err := json.NewEncoder(&buf).Encode(p)
+	if err != nil {
+		return "", err
+	}
+	response, err := http.Post(config.SlackWebhookURL, "application/json", &buf)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+	spew.Dump(err)
+	return "success", nil
 
 	// response, err := http.Post("")
 }
